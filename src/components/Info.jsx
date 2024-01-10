8cfbcd0699be40df1ea5d43e37a3a30acdb833f7
Katjahkrogh <you@example.com>
2024-01-10 15:44:36 +0100

    info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -147,10 +147,10 @@ function Info({ tickets, setStep }) {
           className="font-medium enabled:bg-fooPink-900 aria-disabled:bg-fooPink-900 aria-disabled:opacity-50 p-4 px-8 rounded-full w-full md:w-fit mt-10 transition ease-in-out enabled:hover:-translate-y-1 enabled:hover:scale-110 enabled:hover:bg-fooPink-800 duration-300 enabled:cursor-pointer aria-disabled:cursor-not-allowed
               "
           aria-disabled={
-            !infoForms.every(
-              (form) =>
-                form.firstName && form.lastName && form.email && form.phone
-            )
+            infoForms.firstName === "" ||
+            infoForms.lastName === "" ||
+            infoForms.email === "" ||
+            infoForms.phone === ""
           }
           onClick={handleBtn}
         >
